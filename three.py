cd7e460ad0cb5e59c11fa97406fdec72af756e07
Sunjg0 <Sunjg0@163.com>
2021-08-05 14:54:37 +0800

    three

--- three.py
+++ three.py
@@ -1,37 +1,98 @@
-'''
- 1、随机点名
- 2、随机生成棒棒糖
-容器类型：
-    int、str、double boolean
-     元组：()------tuple 不可修改
-     列表[]: 可以修改的
-     字典{ ""   :  "",}键值对的关系
-            键      值
-            通过调用键来获取值
-    姐就是女王 :
-    选择人物： 1、普通  2、初始3个   3、不会减少
-    需求：
-    第一步：初始值：1 ：赢：20 输：0
-    进行计算 : 随机三个数字（1,7）
+'''姐就是女王:
+随机选择人物： 1、普通没有技能
+2、稀有英雄初始值为30。3、传奇英雄不会减少只会增加
+需求：
+初始值为：10
+随机生成三个数字
+随机选择一个数字
+选择的数字和初始值进行计算（随机加减）
+计算过后大于100则任务成功
+计算过后小于或等于0则任务失败'''
+import random
 
-'''
-
-#增加一个代码判断之后结束   break#跳出循环
-import random  #生成随机数的模块
-name = ["王佳琪", "牛埔至", "王天旭", "小宝", "陆经理"]   #是个列表   内容是个范围
-i = 0 #的初始值为0
-print("第一个",i)
-while i<10: #进行判断：当i<2时运行下面的代码
-    i+=1   #i  = i +1   结束整个循环  i=>结束
-    chose=input("请输入您的业务（1、点名2、分发棒棒糖）") #从键盘上获取的是str 赋值为chose
-    print("第二个",i)
-    if chose == "1":   #一个等于号是赋值  两个是判断  chose ==1  运行下面的代码 如果chose不等于1 跳过
-        a=len(name)   #a获取到的数字 0 1 2 3 4
-        index=random.randint(0,a-1)  #随机生成的角标 a:int,B:int
-        print(name[index])
-    elif chose =="2":  #如果第一个判断跳过 执行第二个判断 chose==2 如果是运行下面的代码 如不等于2 跳过
-        num=random.randint(5,10)
-        print("获得了",num,"个棒棒糖")
-    else: #如果第二个判断跳过 直接执行下面的代码
-        print("你错了")
-print(name[index],"获得了",num,"个棒棒糖")
+list1 = ["普通", "稀有", "传奇"]  # ,"稀有","传奇"
+# 0     1     2    3 4
+#        列表   [                0--------4随机取数字]数字
+# print(list1[random.randint(0,len(list1)-1)])
+#      列表1    随机数       开始 ，结束（通过这个列表里面有多少元素）
+name = list1[random.randint(0, len(list1) - 1)]
+print(name)
+num = 50
+if name == "普通":
+    while True:
+        i = 0  # 从0开始
+        list2 = []
+        while i < 3:  # 判断 i是否小于三 i小于三运行下面的代码
+            i = i + 1  # 每次循环都加1
+            a = random.randint(5, 20)
+            list2.append(a)
+        # while = 初始值 while判断这个初始值是否大于一个值
+        print(list2)
+        num1 = list2[random.randint(0, len(list2) - 1)]  # 这是两边随机取出来的数字num1
+        print(num1)
+        list3 = [1, 2]  # 1 ==加号  2 等于减号
+        name2 = list3[random.randint(0, len(list3) - 1)]  # 随机的加减
+        if name2 == 1:  # 随机的加减如果等于1 进行加法运算
+            num = num1 + num
+            print(num)  # 初始num 加上一个随机取出来的数字
+        elif name2 == 2:  # 随机的加减如果等于1 进行减法运算
+            num = num1 - num
+            print(num)  # 初始num 加上一个随机取出来的数字
+        if num > 100:
+            print("任务成功你的分数为", num)
+            break
+        elif num <= 0:
+            print("任务失败你的分数为", num)
+            break
+if name == "稀有":
+    while True:
+        i = 0  # 从0开始
+        list2 = []
+        while i < 3:  # 判断 i是否小于三 i小于三运行下面的代码
+            i = i + 1  # 每次循环都加1
+            a = random.randint(5, 20)
+            list2.append(a)
+        # while = 初始值 while判断这个初始值是否大于一个值
+        print(list2)
+        num1 = list2[random.randint(0, len(list2) - 1)]  # 这是两边随机取出来的数字num1
+        print(num1)
+        list3 = [1, 2]  # 1 ==加号  2 等于减号
+        name2 = list3[random.randint(0, len(list3) - 1)]  # 随机的加减
+        if name2 == 1:  # 随机的加减如果等于1 进行加法运算
+            num = num1 + num
+            print(num)  # 初始num 加上一个随机取出来的数字
+        elif name2 == 2:  # 随机的加减如果等于1 进行减法运算
+            num = num1 - num
+            print(num)  # 初始num 加上一个随机取出来的数字
+        if num > 100:
+            print("任务成功你的分数为", num)
+            break
+        elif num <= 0:
+            print("任务失败你的分数为", num)
+            break
+if name == "传奇":
+    while True:
+        i = 0  # 从0开始
+        list2 = []
+        while i < 3:  # 判断 i是否小于三 i小于三运行下面的代码
+            i = i + 1  # 每次循环都加1
+            a = random.randint(5, 20)
+            list2.append(a)
+        # while = 初始值 while判断这个初始值是否大于一个值
+        print(list2)
+        num1 = list2[random.randint(0, len(list2) - 1)]  # 这是两边随机取出来的数字num1
+        print(num1)
+        list3 = [1, 2]  # 1 ==加号  2 等于减号
+        name2 = list3[random.randint(0, len(list3) - 1)]  # 随机的加减
+        if name2 == 1:  # 随机的加减如果等于1 进行加法运算
+            num = num1 + num
+            print(num)  # 初始num 加上一个随机取出来的数字
+        elif name2 == 2:  # 随机的加减如果等于1 进行减法运算
+            num = num1 - num
+            print(num)  # 初始num 加上一个随机取出来的数字
+        if num > 100:
+            print("任务成功你的分数为", num)
+            break
+        elif num <= 0:
+            print("任务失败你的分数为", num)
+            break
